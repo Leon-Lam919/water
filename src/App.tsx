808196014850a5727635e6873a4b0b065ad4fa41
Leon-Lam919 <leonlam34@gmail.com>
2024-03-08 14:04:28 -0600

    changed some stuff around

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,13 @@
-console.log('Hello world');
+// App.tsx
+import React from 'react';
+
+const Menu: React.FC = () => {
+  return (
+    <body>
+      <div className="bar">
+    </div>
+    </body>
+  );
+}
+
+export default Menu;
